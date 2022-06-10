9cd09fa2662c4146907d0c3c74c1fbc2306fce19
Ruann <ruannhxh1@gmail.com>
2022-06-10 18:52:31 -0300

    Fix navbar

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -10,7 +10,7 @@ const Navbar = () => (
             <li><a href="/">Home</a></li>
             <li><a href="https://github.com/Project-Forrest/Olympus" rel="noreferrer" target="_blank">About</a></li>
             <li><a href="/admin">Admin</a></li>
-            <li><a href="/admin/new-user">New User</a></li>
+            <li><a href="/admin/new-user">New Gym</a></li>
          </ul>
     </nav>
 );
